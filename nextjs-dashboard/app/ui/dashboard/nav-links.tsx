6d91d53d8474ee118035cf792fb046be5d61b684
Jonah Rosner <rosner1@purdue.edu>
2025-04-12 14:50:41 -0400

    Clean up

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,6 +1,5 @@
 'use client';
 import {
-  UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
@@ -13,11 +12,10 @@ import clsx from 'clsx';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    name: 'Generator',
+    href: '/dashboard/generator',
     icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  }
 ];
 
 export default function NavLinks() {
